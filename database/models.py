c50ddf23e91d369fcec919608d7c0658d74f446a
Sena Katako <sk2545@cornell.edu>
2019-03-01 23:40:45 -0500

    modified models.py

--- database/models.py
+++ database/models.py
@@ -1,12 +1,75 @@
 from django.db import models
 
-SHORT_LENGTH = 10
-LONG_LENGTH = 100
+SHORT_LENGTH = 100
+LONG_LENGTH = 2000
+
+
+
+class Farm(models.Model):
+
+	"""
+        This class describes the profile of the farm.  
+    """
+
+    farm_id_pk = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=SHORT_LENGTH, blank=True)
+    bio = models.CharField(max_length=LONG_LENGTH)
+    area = models.DecimalField() #in acres
+    location = models.JSONField() #json of {longitude, latitude}
+    crops_grown = models.JSONField() #json of {list of crop ids}
+    interested_in_selling = models.BooleanField()
+    contact = models.JSONField() #json of {person of contact name, phone, email}
 
 
 class Crop(models.Model):
-    """This will represent each crop
+
     """
+
+    This table will represent each crop
+
+    """
+
+    crop_id_pk = models.AutoField(primary_key=True)
     name = models.CharField(max_length=SHORT_LENGTH)
-    description = models.TextField(max_length=LONG_LENGTH, blank=True)
-    file = models.FileField(upload_to='crops/')
+    start_month = models.IntegerField()
+    end_month = models.IntegerField()
+    family_id_fk = models.ForeignKey(CropFamily, blank=True, null=True, on_delete=models.SET_NULL)
+
+
+
+class CropFamily(models.Model):
+
+	"""
+	This table contains the general name of the crop
+
+	"""
+
+	family_id_pk = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=SHORT_LENGTH)
+	icon = models.CharField(max_length=LONG_LENGTH)
+
+
+class Month(models.Model):
+
+	"""
+	This table stores all the months in a year
+	"""
+
+	month_id_pk = models.AutoField(primary_key=True)
+	crop_id_fk = models.ForeignKey(Crop, blank=True, null=True, on_delete=models.SET_NULL)
+	name = models.CharField(max_length=SHORT_LENGTH)
+	month_number = models.IntegerField()
+
+
+class Disease(models.Model):
+
+	"""
+	This table contains all possible diseases
+	"""
+
+	disease_id_pk = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=SHORT_LENGTH)
+	picture_link = models.CharField(max_length=LONG_LENGTH)
+	symptoms = models.JSONField()
+	crops_affected = models.JSONField() #json of {Secondary keys of crops}
+	treatment = models.JSONField() #json of {organic:, pesticide:}
