477e77732053af0a48710c06afa84ee8da09a230
Samuel Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2019-03-02 16:22:53 -0500

    adding static and media routes

--- afuom/settings.py
+++ afuom/settings.py
@@ -123,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+
+# Simplified static file serving.
+STATICFILES_LOCATION = 'static'
+MEDIAFILES_LOCATION = 'media'
