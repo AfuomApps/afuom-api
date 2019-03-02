181c25850504beadc1d7db34b0aa80b2567d7081
Samuel Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2019-03-02 16:45:30 -0500

    sample data

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.api_welcome_page, name='api_welcome_page'),
+    url(r'^locations$', views.locations, name='locations'),
+
 ]
--- api/views.py
+++ api/views.py
@@ -4,3 +4,26 @@ from django.http import JsonResponse
 
 def api_welcome_page(request):
     return JsonResponse({'team': 'Afuom', 'message': 'We are the coolest team!!!'})
+
+
+def locations(request):
+    return JsonResponse({"locations": [
+        {
+            "alert": "yellow",
+            "threat": "rodents",
+            "longitude": -46.818894,
+            "latitude": -23.007798,
+        },
+        {
+            "alert": "green",
+            "threat": "rodents",
+            "longitude": -46.821657,
+            "latitude": -23.007268
+        },
+        {
+            "alert": "red",
+            "threat": "red spots disease",
+            "longitude": -46.815429,
+            "latitude": -23.007696,
+        },
+    ]})
