03abe7f00cd4ea06a92cfd0d272bae0db5ce58ae
Samuel Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2019-03-01 22:38:14 -0500

    adding dummy model

--- database/models.py
+++ database/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+SHORT_LENGTH = 10
+LONG_LENGTH = 100
+
+
+class Crop(models.Model):
+    """
+        This will represent each crop
+    """
+    name = models.CharField(max_length=SHORT_LENGTH)
+    description = models.TextField(max_length=LONG_LENGTH, blank=True)
+    file = models.FileField(upload_to='/crops/')
